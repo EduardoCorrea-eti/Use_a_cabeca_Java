087034502a09a80b7a197eee8145a227847b2402
Eduardo Corrêa <carloseducorrea10@gmail.com>
2022-07-15 01:59:04 -0300

    Depois do Olá, Mundo...

---
+++ P08.java
@@ -0,0 +1,54 @@
+
+public class P08{
+
+    /**
+     * @param args
+     */
+    public static void main (String[] args){
+        /*===pg08===
+        int x = 3;
+        String name = "Dirk";
+        x = x * 17;
+        System.out.print("x `is " + x);
+        double d = Math.random();
+        int x = 0;
+        while(x > 12){
+            x = x - 1;
+        }
+         
+        for(int x = 0; x < 10; x = x + 1){
+            System.out.print("x is now" + x);
+
+        }=== 
+        
+        ===pg09===
+        int x = 100;
+        while(x >= 3){
+            x = x - 1;
+            System.out.println(x);
+
+        }
+        ************************
+        APONTE SEU LÁPIS
+        ************************
+        int x = 1;
+        while( x < 3 ){
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x=x+1;
+        }
+        if(x == 3){
+            System.out.print("Do");
+
+        }
+        ************************/
+
+        
+
+    
+    
+
+    }
+
+
+}
