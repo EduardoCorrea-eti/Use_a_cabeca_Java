34851e8686b54050d5ebf46c92a33ca7b0d7088d
Eduardo Corrêa <carloseducorrea10@gmail.com>
2022-07-22 22:36:31 -0300

    pag 13

--- P08.java
+++ P08.java
@@ -42,13 +42,64 @@ public class P08{
 
         }
         ************************/
+        /**
+         * Codificando um aplicativo real pg 11
+         */
+        /* 
+        int beerNum = 99;
+        String word = "boottles";
 
-        
+        while(beerNum > 0 ){
 
+            if(beerNum == 1 ){
+
+                word = "boottlle";
+
+            }
+            
+            System.out.println(beerNum + " " + word + " of beer on the wall");
+            System.out.println(beerNum + " " + word + " of beer.");
+            System.out.println("Take one down.");
+            System.out.println("Pass it around.");
+            beerNum = beerNum -1;
+
+            if(beerNum < 0){
+                System.out.println("No more bottlles osf beer on the wall");
+            }
+        }
+   */
+
+    /*==============================================
+     * pagina 13 PhaseMatic
+     */
     
-    
+     String[] wordListOne = {"24/7", "agora vai", "ola mundo", "estou aprendendo Java", "este é um exercício"};
+     String[] wordListTwo = {"programação", "férias", "cerveja", "barulho", "nervoso"};
+     String[] wordListTree = {"atenção", "militar", "foco", "sargento", "acabou", "beber"};
 
-    }
+     int oneLength = wordListOne.length;
+     int twoLength = wordListTwo.length;
+     int treeLength = wordListTree.length;
+
+     int rand1 = (int) (Math.random() * oneLength );
+     int rand2 = (int) (Math.random() * twoLength );
+     int rand3 = (int) (Math.random() * treeLength );
+
+     String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListTree[rand3];
+
+
+     System.out.println("Precisamos de " + phrase);
+
+
+
+
+
+
+
+
+
+}
+
 
 
 }
